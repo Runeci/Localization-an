7f07de6b9fb69f11879ca4fdfef9be1d1cbceb86
daryia_kostenok <d.kastsianok@gmail.com>
2022-12-24 11:58:29 +0300

    bla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'localize';
+
+  localeList = [
+    {code: 'es-US', label: 'En'},
+    {code: 'ru', label: 'Ru'},
+  ]
 }
